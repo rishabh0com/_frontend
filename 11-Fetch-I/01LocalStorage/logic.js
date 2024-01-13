eb007ee784050af86ea36c4dbf5a45de5630ff41
Rishabh Tripathi <118434225+rishabh0com@users.noreply.github.com>
2024-01-13 06:29:05 +0000

    review the code

--- 11-Fetch-I/01LocalStorage/logic.js
+++ 11-Fetch-I/01LocalStorage/logic.js
@@ -2,15 +2,15 @@ let myform = document.querySelector("form");
 let name_ = document.getElementById("name");
 let age = document.getElementById("age");
 
-myform.addEventListener("submit",(event)=>{
+myform.addEventListener("submit", (event) => {
     event.preventDefault();
-    localStorage.setItem("Name",name_.value);
-    localStorage.setItem("Age",age.value);
+    localStorage.setItem("Name", name_.value);
+    localStorage.setItem("Age", age.value);
 
 })
 let Locbtn = document.querySelector("#fetchLoc")
 
-Locbtn.addEventListener("click",()=>{
+Locbtn.addEventListener("click", () => {
     let locName = localStorage.getItem("Name");
     let locage = localStorage.getItem("Age");
 
@@ -21,7 +21,7 @@ Locbtn.addEventListener("click",()=>{
     td1.textContent = locName
     td2.textContent = locage
 
-    displayLoc.append(td1,td2)
+    displayLoc.append(td1, td2)
 })
 
 
